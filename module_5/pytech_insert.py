95e8ff6caa20e29535707dab233039f0361fcfc7
terrylevans <99369029+terrylevans@users.noreply.github.com>
2022-07-05 19:38:10 -0500

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,1 +1,9 @@
+use('pytech')
+
+db.students.insert({'_id':1007, 'first_name':'John', 'last_name': 'Doe'})
+
+db.students.insert({'_id':1008, 'first_name':'Jane', 'last_name': 'Doe'})
+
+db.students.insert({'_id':1009, 'first_name':'Alfred', 'last_name': 'Hitchcock'})
+
 
