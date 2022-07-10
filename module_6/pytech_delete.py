e992251614f26f1aa18e8129c5ed5aca60618a77
terrylevans <99369029+terrylevans@users.noreply.github.com>
2022-07-10 16:56:17 -0500

    Update pytech_delete.py

--- module_6/pytech_delete.py
+++ module_6/pytech_delete.py
@@ -1,1 +1,24 @@
+use ('pytech')
 
+db.students.deleteOne({_id: ObjectId("62cb330549f51d4e20e0f689")})
+
+db.students.find()
+
+Output:
+  [
+  {
+    "_id": 1007,
+    "first_name": "Robert",
+    "last_name": "Doe"
+  },
+  {
+    "_id": 1008,
+    "first_name": "Jane",
+    "last_name": "Doe"
+  },
+  {
+    "_id": 1009,
+    "first_name": "Alfred",
+    "last_name": "Hitchcock"
+  }
+]
