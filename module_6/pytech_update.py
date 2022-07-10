7cb513f7055c787aea6a1f65dec41499095f1f11
terrylevans <99369029+terrylevans@users.noreply.github.com>
2022-07-10 16:27:57 -0500

    Create pytech_update.py

---
+++ module_6/pytech_update.py
@@ -0,0 +1,10 @@
+use ('pytech')
+db.students.updateOne({first_name:"John"},{$set: {first_name:"Robert"}})
+
+
+
+Output:
+_id:62cb330549f51d4e20e0f689
+student_id:1010
+first_name:"Robert"
+lasnt_name:"Kennedy"
