61a0a8b8ce9e5993e4b50d5edc9edf23d0bbd91f
terrylevans <99369029+terrylevans@users.noreply.github.com>
2022-07-31 13:54:04 -0500

    Update pysports_queries.py

--- module_8/pysports_queries.py
+++ module_8/pysports_queries.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import errorcode
 
 """ database config object """
 config = {
@@ -35,17 +34,6 @@ try:
         print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team ID: {}\n".format(player[0], player[1], player[2], player[3]))
 
     input("\n\n  Press any key to continue... ")
-
-except mysql.connector.Error as err:
-
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("  The supplied username or password are invalid")
-
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("  The specified database does not exist")
-
-    else:
-        print(err)
 
 finally:
     
