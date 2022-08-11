8687379d618fe936b232dd7d6c3d8edcf0613b31
terrylevans <99369029+terrylevans@users.noreply.github.com>
2022-08-11 16:15:20 -0500

    Update whatabook_init.sql

--- module_10/whatabook_init.sql
+++ module_10/whatabook_init.sql
@@ -1,3 +1,52 @@
+DROP USER IF EXISTS 'whatabook_user'@'localhost';
+
+CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MYSQL8IsGreat!';
+
+GRANT ALL PRIVILEGES ON whatabook.* TO 'whatabook_user'@'localhost';
+
+ALTER TABLE wishlist DROP FOREIGN KEY fk_book;
+ALTER TABLE wishlist DROP FOREIGN KEY fk_user;
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS wishlist;
+
+CREATE TABLE store (
+    store_id INT NOT NULL AUTO_INCREMENT,
+    locale VARCHAR(500) NOT NULL,
+    PRIMARY KEY(store_id)
+);
+
+CREATE TABLE book (
+    book_id INT NOT NULL AUTO_INCREMENT,
+    book_name VARCHAR(200) NOT NULL,
+    author VARCHAR(200) NOT NULL,
+    details VARCHAR(500),
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE user (
+    user_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(75) NOT NULL,
+    last_name VARCHAR(75) NOT NULL,
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE wishlist (
+    wishlist_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    PRIMARY KEY(wishlist_id),
+    CONSTRAINT fk_book
+    FOREIGN KEY (book_id)
+        REFERENCES book(book_id),
+    CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+        REFERENCES user(user_id)
+);
+    
+    
 
 INSERT INTO book(book_name, author, details)
     VALUES('The Blade Itself', 'Joe Abercrombie', 'Book One of the First Law Trilogy');
